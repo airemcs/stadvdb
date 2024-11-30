0f5af27bcd2550c2bb93b7ff8632a01cabed140d
el0002 <el.fancubit@gmail.com>
2024-12-01 06:35:56 +0800

    fixed some stuff

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 'use client';
 import Link from "next/link";
+import Router from "next/router";
 import { useEffect, useState } from "react";
 import GameRow from "./components/GameRow";
 
@@ -11,18 +12,20 @@ export default function Home() {
   const [totalPages, setTotalPages] = useState("");
   const [filters, setFilters] = useState({ appId: '', gameName: '', date: '', price: '', requiredAge: '', estimatedOwners: '' });
   const [appliedFilters, setAppliedFilters] = useState({});
+
   const [totalGames, setTotalGames] = useState("");
+  
+  const [showModal, setShowModal] = useState(false); // Modal state
+  const [gameToDelete, setGameToDelete] = useState(null);
   const [selectedNode, setSelectedNode] = useState(() => {
     return window?.localStorage.getItem('selectedNode') || 'main_node';
   });
-  const [showModal, setShowModal] = useState(false);
-  const [gameToDelete, setGameToDelete] = useState(null);
 
-  const gamesPerPage = 5;
+  const gamesPerPage = 6;
 
   const handleDelete = async () => {
     if (!gameToDelete) return;
-
+    setLoading(true);
     try {
       const response = await fetch(`/api/games`, {
         method: 'DELETE',
@@ -37,31 +40,26 @@ export default function Home() {
 
       const data = await response.json();
 
+      setTotalGames(totalGames-1)
+
       if (response.ok) {
         console.log('Game deleted successfully:', data);
         // Update the local state to remove the deleted game
         setGames((prevGames) => prevGames.filter((game) => game.AppID !== gameToDelete));
         setShowModal(false); // Close the modal after deletion
+        setLoading(false);
       } else {
         console.error('Error deleting game:', data.message);
       }
     } catch (error) {
       console.error('Error during delete game process:', error);
     }
-  };
-
-  const handleFilterChange = (e) => {
-    const { name, value } = e.target;
-    setFilters(prev => ({ ...prev, [name]: value }));
-  };
-
-  const applyFilters = () => {
-    setAppliedFilters(filters);
-    setCurrentPage(1);
-    setFilters({ appId: '', gameName: '', price: '', requiredAge: '', estimatedOwners: '', year: '' });
+    
   };
 
   useEffect(() => {
+
+    
     const fetchGames = async () => {
       setLoading(true);
       try {
@@ -76,27 +74,48 @@ export default function Home() {
         const data = await response.json();
         setGames(data.games);
         setTotalGames(data.count)
+
+
         if (data.error === "Failed to fetch games")
           setTotalGames(0)
         setTotalPages(Math.ceil(data.count / gamesPerPage))
       } catch (error) {
         console.error("Error fetching games:", error);
       } finally {
+        setShowModal(false);
         setLoading(false);
       }
     };
 
     fetchGames();
+    
   }, [currentPage, appliedFilters, selectedNode]);
+
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilters(prev => ({ ...prev, [name]: value }));
+  };
 
   useEffect(() => {
     window?.localStorage.setItem('currentPage', currentPage);
   }, [currentPage]);
+  
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      applyFilters();
+    }
+  };
 
   useEffect(() => {
     window?.localStorage.setItem('selectedNode', selectedNode);
     window?.localStorage.setItem('currentPage', currentPage);
   }, [selectedNode, currentPage]);
+
+  const applyFilters = () => {
+    setAppliedFilters(filters);
+    setCurrentPage(1);
+    setFilters({ appId: '', gameName: '', price: '', requiredAge: '', estimatedOwners: '', year: '' });
+  };
 
   const nextPage = () => {
     if (currentPage < totalPages) {
@@ -109,19 +128,12 @@ export default function Home() {
       setCurrentPage(currentPage - 1);
     }
   };
+  
 
   return (
     <>
-      {loading && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
-          <div className="bg-white p-5 rounded-md shadow-xl">
-            <p className="text-gray-800">Loading...</p>
-          </div>
-        </div>
-      )}
-      
-      {/* Confirmation Modal */}
-      {showModal && (
+
+{showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
           <div className="bg-white p-5 rounded-md shadow-xl max-w-md w-full">
             <p className="text-gray-800 text-xl mb-4">Are you sure you want to delete this game?</p>
@@ -143,10 +155,17 @@ export default function Home() {
         </div>
       )}
 
+      {loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+          <div className="bg-white p-5 rounded-md shadow-xl">
+            <p className="text-gray-800">Loading...</p>
+          </div>
+        </div>
+        
+      )}
       <div className="mx-4 lg:mx-16 mt-10 pb-6">
         <div className="text-white text-4xl lg:text-5xl italic font-bold">Games</div>
-
-        {/* Filters */}
+        
         <div className="flex gap-3 justify-center items-center">
           <div className="mt-5">
             <select
@@ -154,133 +173,117 @@ export default function Home() {
               onChange={(e) => setSelectedNode(e.target.value)}
               className="border border-white rounded-md py-2 px-3 text-black"
             >
-              <option value="main_node">Main Node</option>
+              <option value = "main_node">Main Node</option>
               <option>Node1</option>
               <option>Node2</option>
             </select>
           </div>
-          <div className="mt-5 text-xl font-bold">Concurrency</div>
+          <div className="mt-5 text-xl font-bold">
+            Concurrency
+          </div>
           <div className="mt-5 gap-4 flex">
-            <Link href="/concurrency/1" className="border border-white rounded-md py-[9px] px-3 text-black">Case 1</Link>
-            <Link href="/concurrency/2" className="border border-white rounded-md py-[9px] px-3 text-black">Case 2</Link>
-            <Link href="/concurrency/3" className="border border-white rounded-md py-[9px] px-3 text-black">Case 3</Link>
+            <Link href = "/concurrency/1" className="border border-white rounded-md py-[9px] px-3 text-black">Case 1</Link>
+            <Link href = "/concurrency/2" className="border border-white rounded-md py-[9px] px-3 text-black">Case 2</Link>
+            <Link href = "/concurrency/3" className="border border-white rounded-md py-[9px] px-3 text-black">Case 3</Link>
           </div>
         </div>
 
-        {/* Filter Inputs */}
-        <div className="flex flex-wrap items-center space-x-4 mt-5">
-          <div className="flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700">
-            <input
-              value={filters.appId}
-              maxLength="7"
-              type="number"
-              placeholder="App ID"
-              name="appId"
-              className="text-white w-full lg:w-[140px] outline-none bg-transparent"
-              onChange={handleFilterChange}
-            />
+        <div className='flex flex-wrap items-center space-x-4 mt-5'>
+          <div className='flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700'>
+            <input value={filters.appId} maxLength="7" type="number" placeholder="App ID" name="appId" className='text-white w-full lg:w-[140px] outline-none bg-transparent' onChange={handleFilterChange} onKeyPress={handleKeyPress} />
+            <svg className='text-white w-5 h-5 mr-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4a6 6 0 100 12 6 6 0 000-12zm13 15l-7.5-4.5" />
+            </svg>
           </div>
 
-          <div className="flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700">
-            <input
-              value={filters.gameName}
-              type="text"
-              placeholder="Game Name"
-              name="gameName"
-              className="text-white w-full lg:w-[236px] outline-none bg-transparent"
-              onChange={handleFilterChange}
-            />
+          <div className='flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700'>
+            <input value={filters.gameName} type="text" placeholder="Game Name" name="gameName" className='text-white w-full lg:w-[236px] outline-none bg-transparent' onChange={handleFilterChange} onKeyPress={handleKeyPress} />
+            <svg className='text-white w-5 h-5 mr-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4a6 6 0 100 12 6 6 0 000-12zm13 15l-7.5-4.5" />
+            </svg>
           </div>
 
-          <input
-            type="number"
-            value={filters.year}
-            name="year"
-            className="w-full lg:w-[140px] border border-white py-2 px-3 text-white rounded-md bg-transparent hover:border-green-700 hover:shadow focus:border-green-700"
-            onChange={handleFilterChange}
-            min="1900"
-            max="2100"
-            placeholder="Year"
-          />
+          <input type="number" value={filters.year} name="year" className='w-full lg:w-[140px] border border-white py-2 px-3 text-white rounded-md bg-transparent hover:border-green-700 hover:shadow focus:border-green-700' onChange={handleFilterChange} min="1900" max="2100" placeholder="Year" onKeyPress={handleKeyPress} />
 
-          <div className="flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700">
-            <input
-              value={filters.price}
-              type="number"
-              placeholder="Price"
-              name="price"
-              className="text-white w-full lg:w-[140px] outline-none bg-transparent"
-              onChange={handleFilterChange}
-            />
+          <div className='flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700'>
+            <input value={filters.price} type="number" placeholder="Price" name="price" className='text-white w-full lg:w-[140px] outline-none bg-transparent' onChange={handleFilterChange} onKeyPress={handleKeyPress} />
+            <svg className='text-white w-5 h-5 mr-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4a6 6 0 100 12 6 6 0 000-12zm13 15l-7.5-4.5" />
+            </svg>
           </div>
 
-          <div className="flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700">
-            <input
-              value={filters.requiredAge}
-              type="number"
-              placeholder="Required Age"
-              name="requiredAge"
-              className="text-white w-full lg:w-[140px] outline-none bg-transparent"
-              onChange={handleFilterChange}
-            />
+          <div className='flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700'>
+            <input value={filters.requiredAge} type="number" placeholder="Required Age" name="requiredAge" className='text-white w-full lg:w-[140px] outline-none bg-transparent' onChange={handleFilterChange} onKeyPress={handleKeyPress} />
+            <svg className='text-white w-5 h-5 mr-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4a6 6 0 100 12 6 6 0 000-12zm13 15l-7.5-4.5" />
+            </svg>
           </div>
 
-          <div className="flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700">
-            <input
-              value={filters.estimatedOwners}
-              type="number"
-              placeholder="Estimated Owners"
-              name="estimatedOwners"
-              className="text-white w-full lg:w-[140px] outline-none bg-transparent"
-              onChange={handleFilterChange}
-            />
+          <div className='flex items-center border border-white rounded-md px-3 py-2 hover:border-green-700 hover:shadow-lg focus-within:border-green-700'>
+            <input value={filters.estimatedOwners} type="number" placeholder="Estimated Owners" name="estimatedOwners" className='text-white w-full lg:w-[140px] outline-none bg-transparent' onChange={handleFilterChange} />
+            <svg className='text-white w-5 h-5 mr-2' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 4a6 6 0 100 12 6 6 0 000-12zm13 15l-7.5-4.5" />
+            </svg>
           </div>
 
-          <button
-            onClick={applyFilters}
-            className="border text-white rounded-md py-2 px-4 hover:bg-green-700"
-          >
-            Apply Filters
-          </button>
+          <button className='transition duration-300 ease-in-out bg-gray-400 hover:bg-gray-600 text-white w-full lg:w-[202px] py-2 px-5 rounded-md mt-2 lg:mt-0' onClick={applyFilters}>Filter</button>
+
+        </div>
+        <div className="flex w-full flex-col mt-5">
+          <div className="w-full grid grid-cols-2 md:grid-cols-7 text-white text-2xl lg:text-2xl font-bold">
+            <div className="flex justify-center md:col-span-2"></div>
+            <div className="hidden md:flex justify-center"></div>
+            <div className="hidden md:flex justify-center">Total Games: {totalGames}</div>
+            <div className="hidden md:flex justify-center"></div>
+            <div className="hidden md:flex justify-center"></div>
+        </div>
+        </div>
+        <div className="flex w-full flex-col mt-5">
+          <div className="w-full grid grid-cols-2 md:grid-cols-7 text-white text-lg lg:text-xl font-bold">
+            <div className="flex justify-center">App ID</div>
+            <div className="flex justify-center">Game Name</div>
+            <div className="hidden md:flex justify-center">Date</div>
+            <div className="hidden md:flex justify-center">Price</div>
+            <div className="hidden md:flex justify-center">Required Age</div>
+            <div className="hidden md:flex justify-center">Estimated Owners</div>
         </div>
 
-        {/* Game List */}
-        {games &&
-          games.map((game) => (
-            <Link key={game.AppID} href={`/view/${game.AppID}`}>
-              <GameRow
-                appID={game.AppID}
-                gameName={game.Name}
-                date={game.ReleaseDate}
-                price={game.Price}
-                requiredAge={game.RequiredAge}
-                estimatedOwners={game.EstimatedOwners}
-                node={selectedNode}
-                onDelete={() => {
-                  setGameToDelete(game.AppID);
-                  setShowModal(true);
-                }}
-              />
-            </Link>
-          ))}
+          <hr className="border-white mt-3"></hr>
 
-        {/* Pagination */}
-        <div className="flex justify-center mt-5">
-          <button
-            onClick={prevPage}
-            disabled={currentPage === 1}
-            className="border py-2 px-4 rounded-md hover:bg-green-700 disabled:bg-gray-500"
-          >
-            Previous
-          </button>
-          <span className="px-4 py-2">{currentPage} / {totalPages}</span>
-          <button
-            onClick={nextPage}
-            disabled={currentPage === totalPages}
-            className="border py-2 px-4 rounded-md hover:bg-green-700 disabled:bg-gray-500"
-          >
-            Next
-          </button>
+          {games &&
+              games.map((game) => (
+                <Link key={game.AppID} href={`/view/${game.AppID}` }>
+                  <GameRow
+                    appID={game.AppID}
+                    gameName={game.Name}
+                    date={game.ReleaseDate}
+                    price={game.Price}
+                    requiredAge={game.RequiredAge}
+                    estimatedOwners={game.EstimatedOwners}
+                    node = {selectedNode}
+                    onDelete={() => {
+                      setGameToDelete(game.AppID);
+                      setShowModal(true);}} // Correctly pass the parameters
+                  />
+                </Link>
+              ))
+            }
+          <div className="flex justify-center mt-5 space-x-4">
+            <button
+              onClick={prevPage}
+              disabled={currentPage === 1}
+              className={`transition duration-300 ease-in-out bg-gray-700 hover:bg-gray-600 text-white w-full lg:w-[202px] py-2 px-5 rounded-md ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : ''}`}
+            >Previous
+            </button>
+
+            <button
+              onClick={nextPage}
+              disabled={currentPage === totalPages}
+              className={`transition duration-300 ease-in-out bg-gray-400 hover:bg-gray-600 text-white w-full lg:w-[202px] py-2 px-5 rounded-md ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : ''}`}
+            >
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </>
